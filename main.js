9de7ed46b9acf05dcb481cc9c783e0f367f71980
VukIG <vuk.ignjatovic05@gmail.com>
2022-08-20 16:23:06 -0800

    a

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 const board=document.querySelector(".wholething");
 const btn=document.querySelector(".test");
+const buttons=document.querySelectorAll("button");
 
+
+buttons.forEach(button => {
+    button.addEventListener("click",function () {
+        let i=button.innerHTML;
+        draw(i);
+    })
+});
 
 function draw(num) {
+    board.style.gridTemplateColumns=`repeat(${num},1fr)`;
+    board.style.gridTemplateRows=`repeat(${num},1fr)`;
     for (let i = 0; i < Math.pow(num,2); i++) {
         let block = document.createElement("div");
+        block.classList.add("jovan");
         block.style="background-color:blue";
         board.insertAdjacentElement("beforeend",block) 
     }
 }
-
-btn.addEventListener("click",function () {
-    board.style.gridTemplateColumns="repeat(16,1fr)";
-    board.style.gridTemplateRows="repeat(16,1fr)";
-    draw(16);
-})
